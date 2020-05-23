5e94195a96bb62c199e551cee406a9a774e97501
Reynaldo Teixeira <reynaldoteixeira@gmail.com>
2020-05-23 20:18:35 -0300

    Starting 25 minutes countdown configuration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,22 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  workMinutes = 25
+  workSeconds = 0 
+  interval 
+  start(){
+
+    this.interval = setInterval(()=>{
+      if(this.workSeconds == 0){
+        this.workMinutes = this.workMinutes - 1
+        this.workSeconds = 59
+      }
+
+      this.workSeconds = this.workSeconds - 1
+    },1000)
   }
 
 }

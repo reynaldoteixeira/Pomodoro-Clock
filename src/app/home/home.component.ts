ac94178d48f11b00f74671d86cb63942e65ca9f9
Reynaldo Teixeira <reynaldoteixeira@gmail.com>
2020-05-24 13:19:31 -0300

    Rest minutes
    After the Work minutes end the Rest minutes will start run.
    After the Rest minutes end the Work minutes (start function) will work again
    
    Can`t stop run time yet, just pause

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,26 +11,61 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     
+  
   }
 
-  workMinutes = 25
-  workSeconds = 0 
+  workMinutes = 0
+  workSeconds = 3 
+
+  restMinutes = 0
+  restSeconds = 3
+
   interval 
+  format = false
 
   start(){
 
     this.interval = setInterval(()=>{
+
       if(this.workSeconds == 0){
         this.workMinutes = this.workMinutes - 1
         this.workSeconds = 59
       }
-
+    
       this.workSeconds = this.workSeconds - 1
+
+      if(this.workMinutes == 0 && this.workSeconds == 0){
+        this.workMinutes = 25
+        this.workSeconds = 0
+        this.pause()
+        this.rest()
+      }
+
+    },1000)
+  }
+
+
+  rest(){
+    this.interval = setInterval(()=>{
+
+      if(this.restSeconds == 0){
+        this.restMinutes = this.restMinutes - 1
+        this.restSeconds = 59
+      }
+
+      this.restSeconds = this.restSeconds - 1
+
+      if(this.restMinutes == 0 && this.restSeconds == 0){
+        this.restMinutes = 5
+        this.restSeconds = 0
+        this.pause()
+        this.start()
+      }
+      
     },1000)
   }
 
   pause(){
     clearInterval(this.interval)
   }
-
 }
